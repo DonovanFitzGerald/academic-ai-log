c53e4861394c8e01160f732990730aa731a09a53
Donovan FitzGerald <donovan.fitzg@gmail.com>
2026-02-20 12:36:29 +0000

    Test front-end api request

---
+++ resources/js/components/test-ai-chat.tsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+
+const TestAIChat = (props: { className?: string }) => {
+    const [inputText, setinputText] = useState(
+        'Explain how AI works in a few words',
+    );
+    const [chatResponse, setchatResponse] = React.useState(null);
+    const [errorMessage, setErrorMessage] = React.useState(null);
+    const [isLoading, setIsLoading] = React.useState(false);
+
+    const handleInputChange = (e) => {
+        setinputText(e.target.value);
+    };
+
+    const handleButtonPress = async () => {
+        const url =
+            'https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent';
+        console.log('Request prompt:', inputText);
+        try {
+            setIsLoading(true);
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-goog-api-key': 'AIzaSyCgMF-ZTqHq0YbCe6FfoNAjCnog_6u-u7k',
+                },
+                body: JSON.stringify({
+                    contents: [
+                        {
+                            parts: [
+                                {
+                                    text: inputText,
+                                },
+                            ],
+                        },
+                    ],
+                }),
+            });
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            const result = await response.json();
+            console.log(result);
+            const responses = result.candidates[0].content.parts;
+            console.log(responses);
+            setchatResponse(responses);
+            console.log(chatResponse);
+        } catch (error) {
+            setIsLoading(false);
+            console.error(error.message);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return (
+        <div
+            className={`flex h-full w-full items-center justify-center ${props.className ? props.className : ''}`}
+        >
+            <div className="m-auto flex flex-col items-stretch gap-12">
+                {isLoading ? (
+                    <p className="w-fit animate-bounce">Awaiting response...</p>
+                ) : chatResponse ? (
+                    chatResponse.map((r) => <p>{r.text}</p>)
+                ) : (
+                    <p>No response yet</p>
+                )}
+                {errorMessage ? null : (
+                    <p className="text-red-500">{errorMessage}</p>
+                )}
+                <textarea
+                    className="border-2 border-black p-4"
+                    type="textarea"
+                    rows="4"
+                    cols="50"
+                    onChange={(e) => handleInputChange(e)}
+                    value={inputText}
+                ></textarea>
+                <button
+                    className="cursor-pointer rounded-2xl border-black bg-blue-400 px-4 py-4 font-bold text-white"
+                    onMouseDown={() => handleButtonPress()}
+                >
+                    Send prompt
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default TestAIChat;
--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,8 +1,9 @@
 import { Head } from '@inertiajs/react';
+import TestAIChat from '@/components/test-ai-chat';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
-import type { BreadcrumbItem } from '@/types';
 import { dashboard } from '@/routes';
+import type { BreadcrumbItem } from '@/types';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -28,7 +29,7 @@ export default function Dashboard() {
                     </div>
                 </div>
                 <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <TestAIChat />
                 </div>
             </div>
         </AppLayout>
