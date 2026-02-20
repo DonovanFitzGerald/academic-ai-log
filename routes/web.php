4a1368e14890b05042010fa5095ab6a7e1c79d2a
Donovan FitzGerald <donovan.fitzg@gmail.com>
2026-02-20 12:40:28 +0000

    Start of laravel API route

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,28 @@ Route::get('dashboard', function () {
     return Inertia::render('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/ai-request', function () {
+    define('BASE_URL', 'https://api.flickr.com/services');
+
+    $apiKey= $_ENV['API_KEY'];
+    
+    $queryString = http_build_query([
+        'x-goog-api-key' => $apiKey,
+        'content_type' => 1,
+        'format' => 'php_serial',
+        'media' => 'photos',
+        'method' => 'flickr.photos.search',
+        'per_page' => 10,
+        'safe_search' => 1,
+    ]);
+    $requestUri = sprintf(
+        '%s/rest/?%s',
+        BASE_URL,
+        $queryString
+    );
+    $fp = fopen($requestUri, 'r');
+    return response('Hello World', 200)
+        ->header('Content-Type', 'text/plain');
+});
+
 require __DIR__.'/settings.php';
